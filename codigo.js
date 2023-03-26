87f6ca8e32c6716254dbc31a36e0fccf43509519
MiguelRojas-05 <miguelaimar052001@gmail.com>
2023-03-25 19:51:01 -0500

    Hicimos formularios dinamicos con JS y matematicas, además un botón que se mueve xD

--- codigo.js
+++ codigo.js
@@ -37,3 +37,128 @@ function sumarInputsValues(e){
     const sumaIn = In1.value + In2.value;
     resultForm.innerText ='Resultado: '+ sumaIn;
 }
+
+//Calculando descuentos
+const inputPrice = document.querySelector('#price');
+const inputDiscount = document.querySelector('#discount');
+const btnCalcularDiscount = document.querySelector('#calcular');
+const pResultDiscount = document.querySelector('#resultDiscount'); 
+
+btnCalcularDiscount.addEventListener('click',calcularPrecioDescuento);
+
+function calcularPrecioDescuento(e){
+    e.preventDefault();
+
+    const price = Number(inputPrice.value);
+    const discount = Number(inputDiscount.value);
+
+    if(!price || !discount){
+        pResultDiscount.innerText = 'Por favor llena el formulario';    
+    
+        return;
+    }
+    if(discount>60){
+        pResultDiscount.innerText = 'No podemos hacer mas de 60% de descuento';
+        
+        return;
+    }
+        const newPrice = (price *(100-discount))/100;
+
+        pResultDiscount.innerText = 'El nuevo precio con descuento es $'+newPrice;
+    
+    
+}
+
+
+// Descuentos con cupones, implementando método propio
+
+
+const botonCalcularCupon = document.querySelector('#botonCupones');
+const INPUTpriceOrigen = document.querySelector('#priceOrigen');
+const cuponDescuento = document.querySelector('#cuponDescuento');
+const parrafoResultado = document.querySelector('.parrafoResultado')
+const priceOrigen = Number(INPUTpriceOrigen.value);
+
+//Creamos lista de cupones disponible, con sus respectivos descuentos
+const listaCupones = [];
+listaCupones.push({
+    name:'123',
+    descuento:40
+});
+
+listaCupones.push({
+    name:'456',
+    descuento:30
+});
+
+listaCupones.push({
+    name:'789',
+    descuento:20
+});
+
+
+//const codigoCuponDisponible = "platzi123";
+
+
+//Creamos función para calcular el respectivo cupon por cada descuento
+botonCalcularCupon.addEventListener("click",descuentoCupon);
+
+
+botonCalcularCupon.addEventListener("mouseover", moverCondicionalBoton);
+
+function moverCondicionalBoton(){
+    
+     
+    
+    if(!priceOrigen || !cuponDescuento){
+    
+        botonCalcularCupon.classList.toggle('move1');
+    }   
+}
+
+function descuentoCupon (e){
+
+        e.preventDefault();
+
+            
+
+        if(!priceOrigen || !cuponDescuento){
+       //     parrafoResultado.innerText = 'Por favor llena el formulario';    
+    
+        // return;
+
+        
+
+        
+
+        }
+
+        let descuento;
+
+
+//Creamos una función para verificar que coincida lo que escriben los usuarios con lo que tenemos en la lista de cupones
+//Para hacerlo creamos una variable temporal "elementoCupon", y verificamos con el name de cada objeto del arreglo
+
+        function esCuponDeLista(elementoCupon){
+            return elementoCupon.name ==   cuponDescuento.value;
+        }
+
+//Buscamos en la lista de elementos el cupón que coincida con name, y dicho elemento que lo contenga será el devuelto
+        const siCuponLista = listaCupones.find(esCuponDeLista);   
+
+
+        if(siCuponLista){
+
+            descuento = siCuponLista.descuento;
+
+            const nuevoPrecio = (priceOrigen *(100-descuento))/100;
+            parrafoResultado.innerText = "El nuevo precio es $"+nuevoPrecio;
+            return;
+        }
+        else{
+            parrafoResultado.innerText = "Código no válido";            
+        }
+
+    }
+
+
